4d31beccc8d241516f42a590231262b18d4d0e7a
DESKTOP-ALP9J2M\Alumno <Alumno>
2024-11-28 19:41:39 +0100

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { DetalleLibrosComponent } from "./detalle-libros/detalle-libros.component";
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DetalleLibrosComponent],
+  imports: [RouterModule, RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
