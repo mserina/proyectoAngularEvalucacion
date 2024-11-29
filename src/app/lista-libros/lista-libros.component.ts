4da33ed7b026333e9b320c2533d1ac7e49d38e0d
DESKTOP-1CSI9JF\Alumno <dragontero2@gmail.com>
2024-11-29 10:13:13 +0100

    no message

--- src/app/lista-libros/lista-libros.component.ts
+++ src/app/lista-libros/lista-libros.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { LibrosServiciosService } from '../servicios/libros-servicios.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lista-libros',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lista-libros.component.html',
   styleUrl: './lista-libros.component.css'
 })
-export class ListaLibrosComponent {
+export class ListaLibrosComponent{
 
+  items: any[] = []; // Array para almacenar los datos recibidos del servicio
+  titulo: string = 'Todos'; // Título dinámico por defecto
+  mostrarLista: boolean = false; // Controla si se muestra o no la lista
+
+  constructor(private servicioLibro: LibrosServiciosService) {} // Inyección del servicio
+  
+
+  cargarDatos(saga: string): void {
+    
+    this.cambiarTitulo(saga);
+
+    // Llamamos al servicio para obtener los datos al inicializar el componente
+    this.servicioLibro.obtenerLibrosPorSaga(saga).subscribe({
+      next: (data) => {
+        this.items = data; // Cuando se reciben datos
+        this.mostrarLista = true; // Mostramos la lista
+      },
+      error: (error) => {
+        console.error('Error al obtener datos:', error); // Si ocurre un error
+      },
+      complete: () => {
+        console.log('La llamada al servicio ha terminado'); // Cuando el observable se completa
+      }
+    });
+  }
+
+
+  // Método para cambiar el título según la saga seleccionada
+  cambiarTitulo(nuevoTitulo: string): void {
+    this.titulo = nuevoTitulo;
+  }
 }
--- src/app/servicios/libros-servicios.service.ts
+++ src/app/servicios/libros-servicios.service.ts
@@ -1,21 +1,25 @@
 import { Injectable,  } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root'  // Esto hace que el servicio esté disponible en toda la aplicación
 })
 export class LibrosServiciosService {
 
-  private apiUrl = 'http://localhost:4000/libros';
-
+  private apiUrl = 'http://localhost:3000/libros'; // Cambia 'items' por tu recurso
 
   constructor(private http: HttpClient) {}
 
-  getLibros(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
-  }
-
   
+  
+  // Método para obtener libros filtrados por saga
+  obtenerLibrosPorSaga(saga: string): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl).pipe(
+      map((libros) => 
+        saga === 'Todos' ? libros : libros.filter((libro) => libro.saga === saga) // Si la saga es 'Todos', devuelve todos los libros sino devuelve los libros en base a la Saga elegida
+      )
+    );
+  }
 }
